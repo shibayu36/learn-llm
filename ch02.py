f7fc701e5d2fdb59622266c4964b75bb240c654b
shibayu36 <shibayu36@gmail.com>
2026-09-22 18:35:40 +0900

    tokenizer v2

--- ch02.py
+++ ch02.py
@@ -10,36 +10,69 @@ preprocessed = [item.strip() for item in preprocessed if item.strip()]
 print("words count:", len(preprocessed))
 # print(preprocessed[:30])
 
-all_words = sorted(set(preprocessed))
-vocab_size = len(all_words)
-print("vocab_size:", vocab_size)
+# class SimpleTokenizerV1:
+#     def __init__(self, vocab):
+#         self.str_to_int = vocab
+#         self.int_to_str = {i:s for s,i in vocab.items()}
 
-vocab = {token:integer for integer,token in enumerate(all_words)}
+#     def encode(self, text):
+#         preprocessed = re.split(r'([,.:;?_!"()\']|--|\s)', text)
 
-class SimpleTokenizerV1:
+#         preprocessed = [
+#             item.strip() for item in preprocessed if item.strip()
+#         ]
+#         ids = [self.str_to_int[s] for s in preprocessed]
+#         return ids
+
+#     def decode(self, ids):
+#         text = " ".join([self.int_to_str[i] for i in ids])
+#         # Replace spaces before the specified punctuations
+#         text = re.sub(r'\s+([,.?!"()\'])', r'\1', text)
+#         return text
+
+# tokenizer = SimpleTokenizerV1(vocab)
+# text = """"It's the last he painted, you know,"
+#            Mrs. Gisburn said with pardonable pride."""
+# ids = tokenizer.encode(text)
+# print(ids)
+# decoded = tokenizer.decode(ids)
+# print(decoded)
+
+
+all_tokens = sorted(list(set(preprocessed)))
+all_tokens.extend(["<|endoftext|>", "<|unk|>"])
+vocab = {token:integer for integer,token in enumerate(all_tokens)}
+print(len(vocab.items()))
+
+class SimpleTokenizerV2:
     def __init__(self, vocab):
         self.str_to_int = vocab
-        self.int_to_str = {i:s for s,i in vocab.items()}
-
+        self.int_to_str = { i:s for s,i in vocab.items()}
+    
     def encode(self, text):
         preprocessed = re.split(r'([,.:;?_!"()\']|--|\s)', text)
-
+        preprocessed = [item.strip() for item in preprocessed if item.strip()]
         preprocessed = [
-            item.strip() for item in preprocessed if item.strip()
+            item if item in self.str_to_int 
+            else "<|unk|>" for item in preprocessed
         ]
+
         ids = [self.str_to_int[s] for s in preprocessed]
         return ids
-
+        
     def decode(self, ids):
         text = " ".join([self.int_to_str[i] for i in ids])
         # Replace spaces before the specified punctuations
-        text = re.sub(r'\s+([,.?!"()\'])', r'\1', text)
+        text = re.sub(r'\s+([,.:;?!"()\'])', r'\1', text)
         return text
 
-tokenizer = SimpleTokenizerV1(vocab)
-text = """"It's the last he painted, you know,"
-           Mrs. Gisburn said with pardonable pride."""
-ids = tokenizer.encode(text)
-print(ids)
-decoded = tokenizer.decode(ids)
-print(decoded)
+tokenizer = SimpleTokenizerV2(vocab)
+
+text1 = "Hello, do you like tea?"
+text2 = "In the sunlit terraces of the palace."
+
+text = " <|endoftext|> ".join((text1, text2))
+
+print(text)
+print(tokenizer.encode(text))
+print(tokenizer.decode(tokenizer.encode(text)))
