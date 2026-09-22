8bbf2174a7a707a648ddb245677b63451219c045
shibayu36 <shibayu36@gmail.com>
2026-09-22 18:27:12 +0900

    SimpleTokenizerV1

---
+++ ch02.py
@@ -0,0 +1,45 @@
+import re
+
+with open("the-verdict.txt", "r", encoding="utf-8") as f:
+    raw_text = f.read()
+print("Total number of character:", len(raw_text))
+# print(raw_text[:99])
+
+preprocessed = re.split(r'([,.:;?_!"()\']|--|\s)', raw_text)
+preprocessed = [item.strip() for item in preprocessed if item.strip()]
+print("words count:", len(preprocessed))
+# print(preprocessed[:30])
+
+all_words = sorted(set(preprocessed))
+vocab_size = len(all_words)
+print("vocab_size:", vocab_size)
+
+vocab = {token:integer for integer,token in enumerate(all_words)}
+
+class SimpleTokenizerV1:
+    def __init__(self, vocab):
+        self.str_to_int = vocab
+        self.int_to_str = {i:s for s,i in vocab.items()}
+
+    def encode(self, text):
+        preprocessed = re.split(r'([,.:;?_!"()\']|--|\s)', text)
+
+        preprocessed = [
+            item.strip() for item in preprocessed if item.strip()
+        ]
+        ids = [self.str_to_int[s] for s in preprocessed]
+        return ids
+
+    def decode(self, ids):
+        text = " ".join([self.int_to_str[i] for i in ids])
+        # Replace spaces before the specified punctuations
+        text = re.sub(r'\s+([,.?!"()\'])', r'\1', text)
+        return text
+
+tokenizer = SimpleTokenizerV1(vocab)
+text = """"It's the last he painted, you know,"
+           Mrs. Gisburn said with pardonable pride."""
+ids = tokenizer.encode(text)
+print(ids)
+decoded = tokenizer.decode(ids)
+print(decoded)
